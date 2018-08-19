db3504884115f5fbe8be7f60371f7f11ce0f3438
gurudevis <gss@mail.uni-paderborn.de>
2018-08-19 19:57:43 +0200

    New database implementation

---
+++ src/main/java/org/aksw/gerbil/database/NewExperimentDAOImpl.java
@@ -0,0 +1,53 @@
+package org.aksw.gerbil.database;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+
+import org.aksw.gerbil.config.GerbilConfiguration;
+import org.aksw.gerbil.database.ResultNameToIdMapping;
+import org.aksw.gerbil.datatypes.ErrorTypes;
+import org.aksw.gerbil.datatypes.ExperimentTaskResult;
+import org.aksw.gerbil.semantic.vocabs.CUBE;
+import org.aksw.gerbil.semantic.vocabs.GERBIL;
+import org.aksw.gerbil.web.ExperimentTaskStateHelper;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+
+import com.carrotsearch.hppc.IntDoubleOpenHashMap;
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelCon;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.vocabulary.XSD;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.aksw.gerbil.dataid.DataIDGenerator;
+
+
+public class NewExperimentDAOImpl extends NewAbstractExperimentDAO {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(NewExperimentDAOImpl.class);
+
+	
+	 public List<ExperimentTaskResult> getResultsOfExperiment(Model model,Resource experiment) {
+	    	       
+	        model.add(experiment,GERBIL.Experiment , experiment);
+	         List<ExperimentTaskResult> result = this.template.query(GET_EXPERIMENT_RESULTS, parameters);
+	             for (ExperimentTaskResult e : result) {
+	            addAdditionalResults(e);
+	            addSubTasks(e);
+	        }
+	        return result;
+	    }
+	 
+	}
+
+	    
